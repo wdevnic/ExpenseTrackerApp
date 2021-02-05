ac315d3104eabc41c1b3d42dfb5671bb888d409b
Nick Tam <nbilly315@gmail.com>
2021-02-04 21:52:30 -0800

    Remove test expense data

--- src/app.js
+++ src/app.js
@@ -12,25 +12,7 @@
 
 
  const store = configureStore();
-
- store.subscribe(() => {
-  //  console.log(store.getState())
-    const state = store.getState()
-    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
- //   console.log(visibleExpenses)
- })
- 
-
-store.dispatch(addExpense({description: 'Water bill', amount : 4500}))
-store.dispatch(addExpense({description: 'Gas bill', createdAt: 1000}))
-store.dispatch(addExpense({description: 'Rent', amount : 109500}))
-
-/*  store.dispatch(setTextFilter('water'))
-
- setTimeout(() =>{
-    store.dispatch(setTextFilter('bill'))
- }, 3000) */
-
+  
  const jsx = (
     <Provider store={store}>
         <AppRouter />
